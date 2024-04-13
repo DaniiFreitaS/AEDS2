95d6d36c793bdf911f0b406f24881dab7cb8e9c3
DaniiFreitaS <danielfreitaslele14@gmail.com>
2024-04-13 15:40:17 -0300

    gets e sets

---
+++ TP02/Personagem.java
@@ -0,0 +1,133 @@
+import java.util.Date;
+
+class Personagem {
+
+    private String id;
+    private String name;
+    private String[] alternateNames;
+    private String house;
+    private String ancestry;
+    private String species;
+    private String patronus;
+    private boolean hogwartsStaff;
+    private String hogwartsStudent;
+    private String actorName;
+    private boolean alive;
+    private Date dateOfBirth;
+    private int yearOfBirth;
+    private String eyeColour;
+    private String gender;
+    private String hairColour;
+    private boolean wizard;
+
+    public Personagem(){
+
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String[] getAlternateNames() {
+        return alternateNames;
+    }
+    public void setAlternateNames(String[] alternateNames) {
+        this.alternateNames = alternateNames;
+    }
+    public String getHouse() {
+        return house;
+    }
+    public void setHouse(String house) {
+        this.house = house;
+    }
+    public String getAncestry() {
+        return ancestry;
+    }
+    public void setAncestry(String ancestry) {
+        this.ancestry = ancestry;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public String getPatronus() {
+        return patronus;
+    }
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+    public Boolean getHogwartsStaff() {
+        return hogwartsStaff;
+    }
+    public void setHogwartsStaff(boolean hogwartsStaff) {
+        this.hogwartsStaff = hogwartsStaff;
+    }
+    public String getHogwartsStudent() {
+        return hogwartsStudent;
+    }
+    public void setHogwartsStudent(String hogwartsStudent) {
+        this.hogwartsStudent = hogwartsStudent;
+    }
+    public String getActorName() {
+        return actorName;
+    }
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+    public Boolean getAlive() {
+        return alive;
+    }
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+    public String getEyeColour() {
+        return eyeColour;
+    }
+    public void setEyeColour(String eyeColour) {
+        this.eyeColour = eyeColour;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getHairColour() {
+        return hairColour;
+    }
+    public void setHairColour(String hairColour) {
+        this.hairColour = hairColour;
+    }
+    public Boolean getWizard() {
+        return wizard;
+    }
+    public void setWizard(boolean wizard) {
+        this.wizard = wizard;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
