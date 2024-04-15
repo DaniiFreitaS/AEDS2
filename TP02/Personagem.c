ba9a3cfe4ee73bb667cdf76726a12300b271c369
DaniiFreitaS <danielfreitaslele14@gmail.com>
2024-04-15 20:00:56 -0300

    leitura correçao

--- TP02/Personagem.c
+++ TP02/Personagem.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 typedef struct // Cria uma STRUCT para armazenar os dados de uma pessoa
 {
@@ -23,23 +24,55 @@ typedef struct // Cria uma STRUCT para armazenar os dados de uma pessoa
 
 void ImprimePersonagem(Personagem P) // declara o parâmetro como uma struct
 {
-  printf("%s %s %s %s %s %s %s %d %s %s %d %s %d %s %s %s %d", P.id, P.nome, P.alternateNames, P.house, P.ancestry, P.species, P.patronus, P.hogwartsStaff,
+  printf("%s %s %s %s %s %s %s %d %s %s %d %s %d %s %s %s %d\n", P.id, P.nome, P.alternateNames, P.house, P.ancestry, P.species, P.patronus, P.hogwartsStaff,
    P.hogwartsStudent, P.actorName, P.alive, P.dateOfBirth, P.yearOfBirth, P.eyeColour, P.gender, P.hairColour, P.wizard);
 }
 
-void leEntrada(FILE *raw, Personagem P){
+char* lerArquivo(char *s){
+        FILE *raw = fopen("characters.csv", "r");
+        char *resp = malloc(sizeof (char) * 1000);
+        char str[405][500];
+        for(int i = 0; i < 405; i++){
+            fscanf(raw, " %[^\r\n]", str[i]);
+            //printf(" %s\n", str[i]);
+        }
+        fclose(raw);
+        
+        for(int i = 1; i < 40; i++){
+            
+            
+            if(strstr(str[i], s) != NULL){
+                resp = strstr(str[i],s);
+                if (strcmp(resp, str[i]) == 0){
+                    printf("%s\n %s\n", s, str[i]);
+                    strcpy(resp, str[i]);
+                    //printf("%s\n", resp);
+                    i+=500;
+                }
+            }else{
+                printf("oiiii");
+            }
+            
+            
+        }
+        return resp;
+    }
+
+Personagem leEntrada(char *str, Personagem P){
     char* token;
-    char str[500];
-    fscanf(raw, " %[^\r\n]", str);
-    printf("%s\n", str);
-    fscanf(raw, " %[^\r\n]", str);
-    printf("%s\n", str);
+    //char str[500];
+    //fscanf(raw, " %[^\r\n]", str);
+
+    //printf("%s\n", str);
+    //fscanf(raw, " %[^\r\n]", str);
+    //printf("%s\n", str);
     // Divide a string usando a vírgula como delimitador
     token = strtok(str, ";");//erro aqui
-    printf("%s\n", str);
+   // printf("%s\n", token);
     strcpy(P.id, token);
     
     token = strtok(NULL, ";");
+    
     strcpy(P.nome, token);
     token = strtok(NULL, ";");
     strcpy(P.alternateNames, token);
@@ -73,14 +106,31 @@ void leEntrada(FILE *raw, Personagem P){
     strcpy(P.hairColour, token);
     token = strtok(NULL, ";");
     P.wizard = 1;
+    return P;
 }
 
 int main()
 {
-    FILE *arquivo = fopen("characters.csv", "r");
+    char arquivo[500];
+    char entrada[500];
+    int fim = 0;
     Personagem P;
-    leEntrada(arquivo, P);
-    fclose(arquivo);
+    while(fim == 0){
+        scanf(" %[^\r\n]", entrada);//pega a linha inteira incluindo os espaços
+        if(strcmp(entrada, "FIM") == 0)
+          fim = 1;
+        else{
+            strcpy(arquivo,lerArquivo(entrada));
+            
+            P = leEntrada(arquivo, P);
+            ImprimePersonagem(P);
+        }
+    }
+    //scanf(" %[^\r\n]", &entrada);
+    //strcpy(arquivo,lerArquivo("9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8"));
+    //Personagem P;
+    //P = leEntrada(arquivo, P);
+    //fclose(arquivo);
     /*
     strcpy(P.id, "9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8");
     strcpy(P.nome, "Harry Potter");
@@ -101,6 +151,6 @@ int main()
     P.wizard = 1;
     */
     // chama a função que recebe a struct como parâmetro
-    ImprimePersonagem(P);
+    //ImprimePersonagem(P);
    return 0;
 }
