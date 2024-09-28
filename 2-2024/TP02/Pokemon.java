a0923662e9f05ed10b127ddbdda4719504f0f78b
DaniiFreitaS <danielfreitaslele14@gmail.com>
2024-09-27 23:06:34 -0300

    classe pokemon

--- 2-2024/TP02/Pokemon.java
+++ 2-2024/TP02/Pokemon.java
@@ -1,3 +1,99 @@
-public class Pokemon {
-    
+import java.io.File;
+import java.util.*;
+class Pokemon {
+    //id,generation,name,description,type1,type2,abilities,weight_kg,height_m,capture_rate,is_legendary,capture_date
+    private int id;
+    private int generation;
+    private String name;
+    private String description;
+    private List<String> type;
+    private List<String> abilities;
+    private double weight;
+    private double height;
+    private int capture_rate;
+    private boolean is_legendary;
+    private Date captureDate;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getGeneration() {
+        return generation;
+    }
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<String> getType() {
+        return type;
+    }
+    public void setType(List<String> type) {
+        this.type = type;
+    }
+    public List<String> getAbilities() {
+        return abilities;
+    }
+    public void setAbilities(List<String> abilities) {
+        this.abilities = abilities;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public int getCapture_rate() {
+        return capture_rate;
+    }
+    public void setCapture_rate(int capture_rate) {
+        this.capture_rate = capture_rate;
+    }
+    public boolean isIs_legendary() {
+        return is_legendary;
+    }
+    public void setIs_legendary(boolean is_legendary) {
+        this.is_legendary = is_legendary;
+    }
+    public Date getCaptureDate() {
+        return captureDate;
+    }
+    public void setCaptureDate(Date captureDate) {
+        this.captureDate = captureDate;
+    }
+    public Pokemon(){
+
+    }
+    public static void main(String[] args) {
+        try {
+            File csv = new File("pokemon.csv");
+            Scanner sc = new Scanner(csv);
+            while(sc.hasNext()){
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
+    }
 }
